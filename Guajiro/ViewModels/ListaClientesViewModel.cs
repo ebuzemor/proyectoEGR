9069bc8d992aa77b995affe17ddf3f26f8efb544
Elí Bulmaro Zenteno Moreno <ebuzemor@gmail.com>
2018-01-16 19:05:05 -0600

    cambios en listaclientes

--- Guajiro/ViewModels/ListaClientesViewModel.cs
+++ Guajiro/ViewModels/ListaClientesViewModel.cs
@@ -7,6 +7,7 @@ using MaterialDesignThemes.Wpf;
 using Guajiro.Common;
 using Guajiro.Models;
 using Guajiro.Views;
+using System.Collections.ObjectModel;
 
 namespace Guajiro.ViewModels
 {
@@ -14,16 +15,32 @@ namespace Guajiro.ViewModels
     {
         #region Commands
         public RelayCommand NuevoClienteCommand { get; set; }
+        public RelayCommand BuscarClienteCommand { get; set; }
+        public RelayCommand MostrarTodosCommand { get; set; }
+        public RelayCommand MostrarUltimosCommand { get; set; }
+        public RelayCommand EditarClienteCommand { get; set; }
+        public RelayCommand BorrarClienteCommand { get; set; }        
         #endregion
 
         #region Variables
         public bd_guajiroEntities GuajiroEF;
+
+        private string _txtBuscar;
+        private ObservableCollection<tbl_personas> _listaClientes;
+
+        public string TxtBuscar { get => _txtBuscar; set { _txtBuscar = value; OnPropertyChanged("TxtBuscar"); } }
+        public ObservableCollection<tbl_personas> ListaClientes { get => _listaClientes; set { _listaClientes = value; OnPropertyChanged("ListaClientes"); } }
         #endregion
 
         #region Constructor
         public ListaClientesViewModel()
         {
             NuevoClienteCommand = new RelayCommand(NuevoCliente);
+            BuscarClienteCommand = new RelayCommand(BuscarCliente);
+            MostrarTodosCommand = new RelayCommand(MostrarTodos);
+            MostrarUltimosCommand = new RelayCommand(MostrarUltimos);
+            EditarClienteCommand = new RelayCommand(EditarCliente);
+            BorrarClienteCommand = new RelayCommand(BorrarCliente);
             GuajiroEF = new bd_guajiroEntities();
         }
         #endregion
@@ -33,6 +50,32 @@ namespace Guajiro.ViewModels
         {
             var vwDatosCliente = new DatosClienteView { };
             var result = await DialogHost.Show(vwDatosCliente, "ListaClientes");
+        }
+
+        private void BuscarCliente(object parameter)
+        {
+            var lista = GuajiroEF.tbl_personas.Where(x => x.razon_social.Contains(TxtBuscar)).ToList();
+            ListaClientes = new ObservableCollection<tbl_personas>(lista);
+        }
+
+        private void MostrarTodos(object parameter)
+        {
+
+        }
+
+        private void MostrarUltimos(object parameter)
+        {
+
+        }
+
+        private void EditarCliente(object parameter)
+        {
+
+        }
+
+        private void BorrarCliente(object parameter)
+        {
+
         }
         #endregion
     }
